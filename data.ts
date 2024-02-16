c448c70c5c3904da89b866ff05077e7f644b0e54
mramadan18 <m.ramadan181199@gmail.com>
2024-02-17 00:19:48 +0200

    update links

--- data.ts
+++ data.ts
@@ -10,131 +10,141 @@ import { IProject, IService, ISkill } from "./type";
 export const services: IService[] = [
   {
     title: "Frontend Development",
-    about: "I can build a beautiful and scalable SPA using <b>HTML</b>, <b>CSS</b>, <b>React</b> and <b>Next.js</b>",
+    about:
+      "I can build a beautiful and scalable SPA using <b>HTML</b>, <b>CSS</b>, <b>React</b> and <b>Next.js</b>",
     Icon: RiComputerLine,
   },
   {
     title: "Responsive Design",
-    about: "You make websites and applications responsive, so they adapt and look great on various devices and screen sizes, including desktops, tablets, and smartphones.",
+    about:
+      "You make websites and applications responsive, so they adapt and look great on various devices and screen sizes, including desktops, tablets, and smartphones.",
     Icon: MdScreenshotMonitor,
   },
   {
     title: "JavaScript Development",
-    about: "You use JavaScript to add interactivity to web pages. This can involve creating animations, handling user input, and making web applications dynamic.",
+    about:
+      "You use JavaScript to add interactivity to web pages. This can involve creating animations, handling user input, and making web applications dynamic.",
     Icon: BiLogoJavascript,
   },
   {
     title: "Performance Optimization",
-    about: "You optimize web pages for speed and performance by minimizing loading times, optimizing images, and implementing techniques like lazy loading and code splitting.",
+    about:
+      "You optimize web pages for speed and performance by minimizing loading times, optimizing images, and implementing techniques like lazy loading and code splitting.",
     Icon: CgPerformance,
   },
   {
     title: "Clean Code",
-    about: "Writing clean, maintainable code that adheres to industry standards and best practices.",
+    about:
+      "Writing clean, maintainable code that adheres to industry standards and best practices.",
     Icon: AiOutlineCode,
   },
   {
     title: "Convert designs to HTML CSS code",
-    about: "I excel at converting designs into <b>HTML</b>, <b>CSS</b> and <b>JS</b> code, bringing creative visions to life on the web with precision and responsiveness.",
+    about:
+      "I excel at converting designs into <b>HTML</b>, <b>CSS</b> and <b>JS</b> code, bringing creative visions to life on the web with precision and responsiveness.",
     Icon: FaArrowsTurnRight,
   },
-]
+];
 
 export const languages: ISkill[] = [
   {
     name: "HTML, CSS",
     level: "90%",
-    Icon: Bs0CircleFill
+    Icon: Bs0CircleFill,
   },
   {
     name: "JS, ES6",
     level: "80%",
-    Icon: Bs0CircleFill
+    Icon: Bs0CircleFill,
   },
   {
     name: "Bootstrap, Tailwindcss",
     level: "97%",
-    Icon: Bs0CircleFill
+    Icon: Bs0CircleFill,
   },
   {
     name: "Sass, TypeScript",
     level: "85%",
-    Icon: Bs0CircleFill
+    Icon: Bs0CircleFill,
   },
   {
     name: "React, NextJs",
     level: "80%",
-    Icon: Bs0CircleFill
+    Icon: Bs0CircleFill,
   },
   {
     name: "Redux, Toolkit",
     level: "87%",
-    Icon: Bs0CircleFill
+    Icon: Bs0CircleFill,
   },
-]
+];
 
 export const tools: ISkill[] = [
   {
     name: "Figma",
     level: "75%",
-    Icon: Bs0CircleFill
+    Icon: Bs0CircleFill,
   },
   {
     name: "Postman",
     level: "70%",
-    Icon: Bs0CircleFill
+    Icon: Bs0CircleFill,
   },
   {
     name: "Git & Github",
     level: "80%",
-    Icon: Bs0CircleFill
+    Icon: Bs0CircleFill,
   },
   {
     name: "Json Server",
     level: "85%",
-    Icon: Bs0CircleFill
+    Icon: Bs0CircleFill,
   },
-]
+];
 
 export const projects: IProject[] = [
   {
     id: 1,
     name: "I-News",
-    description: "a real project with NextJS for an Iraqi channel used HTML, CSS, React, Nextjs, Bootstrap",
+    description:
+      "a real project with NextJS for an Iraqi channel used HTML, CSS, React, Nextjs, Bootstrap",
     image_path: "/images/i-news.png",
-    deployed_url: "https://inews-eight.vercel.app",
+    deployed_url: "https://www.i-news.tv",
     github_url: "https://github.com/mramadan18/inews",
     category: ["nextjs"],
-    key_techs: ["HTML", "CSS", "Bootstrap", "React", "Nextjs"]
+    key_techs: ["HTML", "CSS", "Bootstrap", "React", "Nextjs"],
   },
   {
     id: 2,
-    name: "Well",
-    description: "a real project with NextJS to sell medicines to a pharmacy in Hurghada",
+    name: "Well Medic",
+    description:
+      "a real project with NextJS to sell medicines to a pharmacy in Hurghada",
     image_path: "/images/well.png",
-    deployed_url: "http://167.172.104.141/",
-    github_url: "https://github.com/mramadan18/weShare",
+    deployed_url: "https://well-medic.com",
+    github_url: "https://github.com/mramadan18/well-pharmacy",
     category: ["nextjs"],
-    key_techs: ["HTML", "CSS", "Tailwindcss", "React", "Nextjs"]
+    key_techs: ["HTML", "CSS", "Tailwindcss", "React", "Nextjs"],
   },
   {
     id: 4,
     name: "Quiz App",
-    description: "a training project with HTML, CSS, JS I called the API to get questions",
+    description:
+      "a training project with HTML, CSS, JS I called the API to get questions",
     image_path: "/images/quiz-app.png",
     deployed_url: "https://mramadan18.github.io/Quiz-app",
     github_url: "https://github.com/mramadan18/Quiz-app",
     category: ["vanilla js"],
-    key_techs: ["HTML", "CSS", "JavaScript", "Api"]
+    key_techs: ["HTML", "CSS", "JavaScript", "Api"],
   },
   {
     id: 5,
     name: "WeShare",
-    description: "a training project with HTML, CSS, JS is a social networking site",
+    description:
+      "a training project with HTML, CSS, JS is a social networking site",
     image_path: "/images/weShare.png",
     deployed_url: "https://mramadan18.github.io/weShare/",
     github_url: "https://github.com/mramadan18/weShare",
     category: ["vanilla js"],
-    key_techs: ["HTML", "CSS", "JavaScript"]
+    key_techs: ["HTML", "CSS", "JavaScript"],
   },
-]
+];
