56887b9f4012b38474e185300db68572dccb180a
Mahmoud Ramadan <72145545+mramadan18@users.noreply.github.com>
2025-04-02 20:58:23 +0200

    update deployed URLs for projects in data.ts

--- data.ts
+++ data.ts
@@ -103,7 +103,7 @@ export const projects: IProject[] = [
     description:
       "a real project with NextJS for an Iraqi channel used HTML, CSS, React, Nextjs, Bootstrap",
     image_path: "/images/i-news.png",
-    deployed_url: "https://www.i-news.tv",
+    deployed_url: "https://i-news.tv",
     github_url: "https://github.com/mramadan18/inews",
     category: ["nextjs"],
     key_techs: ["HTML", "CSS", "Bootstrap", "React", "Nextjs"],
@@ -114,7 +114,7 @@ export const projects: IProject[] = [
     description:
       "An educational platform that serves teachers and students, allowing teachers to create an account and upload their courses, assignments, and exams. Students can create an account and enroll in courses, view assignments, and exams.",
     image_path: "/images/erazone.png",
-    deployed_url: "https://www.erazone.org",
+    deployed_url: "https://erazone.org",
     github_url: "",
     category: ["nextjs"],
     key_techs: ["React", "Nextjs", "Redux", "Tailwindcss", "HeroUI"],
@@ -124,7 +124,7 @@ export const projects: IProject[] = [
     name: "Plaissir",
     description: "A flight booking site with a support chatbot.",
     image_path: "/images/plaissir.png",
-    deployed_url: "https://www.plaissir.com/",
+    deployed_url: "https://plaissir.com/",
     github_url: "",
     category: ["nextjs"],
     key_techs: ["React", "Nextjs", "Tailwindcss", "HeroUI"],
